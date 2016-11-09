f2446bf3fe5693a7dad6f89a9b9dcff04255e4b5
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2016-11-09 22:58:35 +0900

    修正

--- src/patterned/HanlderTest.java
+++ src/patterned/HanlderTest.java
@@ -5,7 +5,7 @@ public class HanlderTest {
 	public static void main(String[] args) {
 		NumberHandler nh1 = new NumberHandler("100 処理機", 100);
 		NumberHandler nh2 = new NumberHandler("-50 処理機", -50);
-		RangeHandler rh1 = new RangeHandler("50-99 処理機", 50, 75);
+		RangeHandler rh1 = new RangeHandler("50-75 処理機", 50, 75);
 		RangeHandler rh2 = new RangeHandler("25-49 処理機", 25, 49);
 		EvenHandler eh = new EvenHandler("偶数 処理機");
 
